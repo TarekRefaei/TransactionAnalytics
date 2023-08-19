ecf6b07962356e01370d83c5492d1b5ea0e7674b
TarekRefaei <refa3igroup@gmail.com>
2023-08-19 06:57:25 +0300

    Ended

--- lib/screens/transaction_analytic.dart
+++ lib/screens/transaction_analytic.dart
@@ -3,6 +3,7 @@ import 'package:card_transaction_analytics/widgets/doughnut_widget.dart';
 import 'package:flutter/material.dart';
 import '../data/dummy_data.dart';
 import '../models/transaction.dart';
+import '../widgets/month_selector.dart';
 
 class TransactionAnalyticScreen extends StatelessWidget {
   final List<Transaction> transactions;
@@ -47,63 +48,6 @@ class TransactionAnalyticScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class MonthSelector extends StatefulWidget {
-  const MonthSelector({
-    super.key,
-  });
-
-  @override
-  State<MonthSelector> createState() => _MonthSelectorState();
-}
-
-class _MonthSelectorState extends State<MonthSelector> {
-
-  int selectedMonthIndex = DateTime.now().month - 1;
-
-  void goToPreviousMonth() {
-    setState(() {
-      selectedMonthIndex = (selectedMonthIndex - 1) % months.length;
-    });
-  }
-
-  void goToNextMonth() {
-    setState(() {
-      selectedMonthIndex = (selectedMonthIndex + 1) % months.length;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new),
-          onPressed: () {
-            goToPreviousMonth();
-          },
-        ),
-        Text(
-          months[selectedMonthIndex],
-          style: const TextStyle(
-            fontFamily: 'Lato-Thin',
-            fontSize: 16,
-            color: Colors.black,
-          ),
-        ),
-        IconButton(
-          icon: const Icon(
-              Icons.arrow_forward_ios
-          ),
-          onPressed: () {
-            goToNextMonth();
-          },
-        ),
-      ],
     );
   }
 }
